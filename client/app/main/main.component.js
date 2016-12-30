72206ccbc9e255be1afa690967dcf3df5665609f
Andrew Koroluk <koroluka@gmail.com>
2016-12-30 12:12:22 -0500

    initial commit

---
+++ client/app/main/main.component.js
@@ -0,0 +1,48 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Http } from '@angular/http';
+import { SocketService } from '../../components/socket/socket.service';
+
+@Component({
+  selector: 'main',
+  template: require('./main.html'),
+  styles: [require('./main.scss')],
+})
+export class MainComponent {
+  Http;
+  SocketService;
+  awesomeThings = [];
+  newThing = '';
+
+  static parameters = [Http, SocketService];
+  constructor(_Http_, _SocketService_) {
+    this.Http = _Http_;
+    this.SocketService = _SocketService_;
+  }
+
+  ngOnInit() {
+    this.Http.get('/api/things')
+      .map(res => res.json()
+        // this.SocketService.syncUpdates('thing', this.awesomeThings);
+      )
+      .catch(err => Observable.throw(err.json().error || 'Server error'))
+      .subscribe(things => {
+        this.awesomeThings = things;
+      });
+  }
+
+
+  ngOnDestroy() {
+    this.SocketService.unsyncUpdates('thing');
+  }
+
+  addThing() {
+    if(this.newThing) {
+      this.Http.post('/api/things', { name: this.newThing });
+      this.newThing = '';
+    }
+  }
+
+  deleteThing(thing) {
+    this.$http.delete(`/api/things/${thing._id}`);
+  }
+}
